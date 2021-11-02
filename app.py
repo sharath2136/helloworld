4859a8b569929b9c7e4d6e67a75db51145fcb4ed
sharath2136 <sharathkmr110@gmail.com>
2021-11-02 08:07:03 +0000

    third chalice

---
+++ app.py
@@ -0,0 +1,35 @@
+import logging
+
+from chalice import Chalice
+
+app = Chalice(app_name='demolog')
+# Enable DEBUG logs.
+app.log.setLevel(logging.DEBUG)
+
+
+@app.route('/')
+def index():
+    app.log.debug("This is a debug statement")
+    app.log.error("This is an error statement")
+    return {'hello': 'world'}
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
